539092f66fa3af8c2feedfb0beae98dda2a9d311
DNS <37093+DNS@users.noreply.github.com>
2025-10-03 00:43:39 +0700

    init

---
+++ queen.ps1
@@ -0,0 +1,61 @@
+
+$board = @(
+	@('.','.','.','.'),
+	@('.','.','.','.'),
+	@('.','.','Q','.'),
+	@('.','.','.','.')
+)
+
+function CheckAttack ($x, $y) {
+	# check horizontal, x
+	for ($i=0; $i -lt 4; $i++) {
+		return $board[$i][$y] -eq 'Q'
+	}
+	
+	# check vertical, y
+	for ($i=0; $i -lt 4; $i++) {
+		return $board[$x][$i] -eq 'Q'
+	}
+	
+	# check diagonal
+	
+	
+	return $false
+}
+
+
+
+
+
+for ($x=0; $x -lt 4; $x++) {
+	for ($y=0; $y -lt 4; $y++) {
+		# x
+		for ($i=0; $i -lt 4; $i++) {
+			$a = $board[$x][$i] -eq 'Q'
+			if ($a) { "$x $y" }
+		}
+
+		'--------'
+		# y
+		for ($i=0; $i -lt 4; $i++) {
+			$a = $board[$i][$y] -eq 'Q'
+			if ($a) { "$x $y" }
+		}
+
+		'--------'
+		# diagonal
+
+
+	}
+}
+
+
+<#
+
+top-left -> bottom right
+x++, y++
+
+
+
+
+#>
