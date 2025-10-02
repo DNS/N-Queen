2ed9af4986f336c7284daf11b770a7f7aea9c4cc
DNS <37093+DNS@users.noreply.github.com>
2025-10-03 01:24:25 +0700

    -

--- queen.ps1
+++ queen.ps1
@@ -2,7 +2,7 @@
 $board = @(
 	@('.','.','.','.'),
 	@('.','.','.','.'),
-	@('.','.','Q','.'),
+	@('.','.','.','.'),
 	@('.','.','.','.')
 )
 
@@ -10,12 +10,14 @@ $board = @(
 function CheckAttack ($x, $y) {
 	# check horizontal, x
 	for ($i=0; $i -lt 4; $i++) {
-		return $board[$i][$y] -eq 'Q'
+		$board[$i][$y] = 'Q'
+		#return $board[$i][$y] -eq 'Q'
 	}
 	
 	# check vertical, y
 	for ($i=0; $i -lt 4; $i++) {
-		return $board[$x][$i] -eq 'Q'
+		$board[$x][$i] = 'Q'
+		#return $board[$x][$i] -eq 'Q'
 	}
 	
 	# check diagonal
@@ -27,7 +29,7 @@ function CheckAttack ($x, $y) {
 
 
 
-
+<#
 for ($x=0; $x -lt 4; $x++) {
 	for ($y=0; $y -lt 4; $y++) {
 		# x
@@ -49,7 +51,7 @@ for ($x=0; $x -lt 4; $x++) {
 
 	}
 }
-
+#>
 
 
 
@@ -63,8 +65,8 @@ function PrintBoard () {
 }
 
 
-
-#PrintBoard
+CheckAttack 1 1
+PrintBoard
 
 
 <#
