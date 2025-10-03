3a186bf1da63531014ed16da57e1cd6dcae2b93b
DNS <37093+DNS@users.noreply.github.com>
2025-10-03 08:05:47 +0700

    CheckAttack

--- queen.ps1
+++ queen.ps1
@@ -8,20 +8,37 @@ $board = @(
 
 
 function CheckAttack ($x, $y) {
+	$board[$x][$y] = '?'
+
 	# check horizontal, x
 	for ($i=0; $i -lt 4; $i++) {
-		$board[$i][$y] = 'Q'
+		if ($i -ne $x -and $j -ne $y) {
+			$board[$i][$y] = 'Q'
+		}
 		#return $board[$i][$y] -eq 'Q'
 	}
 	
 	# check vertical, y
 	for ($i=0; $i -lt 4; $i++) {
-		$board[$x][$i] = 'Q'
+		if ($i -ne $x -and $j -ne $y) {
+			$board[$x][$i] = 'Q'
+		}
 		#return $board[$x][$i] -eq 'Q'
 	}
 	
 	# check diagonal
-	
+	for ($i=0; $i -lt 4; $i++) {
+		for ($j=0; $j -lt 4; $j++) {
+			if ($i -ne $x -and $j -ne $y) {
+				$m1 = ($j-$y)/($i-$x)
+				$m2 = -($j-$y)/($i-$x)
+				if ($m1 -eq 1 -or $m2 -eq -1) {
+					$board[$i][$j] = 'Q'
+				}
+
+			}
+		}
+	}
 	
 	return $false
 }
