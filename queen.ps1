52f4db08c76e93b656b94c9be6c525dba271ed63
DNS <37093+DNS@users.noreply.github.com>
2025-10-04 02:14:04 +0700

    -

--- queen.ps1
+++ queen.ps1
@@ -6,26 +6,31 @@ $board = @(
 	@('.','.','.','.')
 )
 
+function PrintBoard ($b) {
+	for ($x=0; $x -lt 4; $x++) {
+		for ($y=0; $y -lt 4; $y++) {
+			[Console]::Write( $b[$x][$y] )
+		}
+		[Console]::Write( "`n" )
+	}
+}
 
-function CheckAttack ($x, $y) {
+function CheckAttack ($b, $x, $y) {
 	#$board[$x][$y] = '?'
+	$board_copy = @() + $b
 
 	# check horizontal, x
 	for ($i=0; $i -lt 4; $i++) {
-		if ($i -ne $x -and $j -ne $y) {
-			#$board[$i][$y] = 'Q'
+		if ($board_copy[$i][$y] -eq 'Q') {
 			return $true
 		}
-		#return $board[$i][$y] -eq 'Q'
 	}
 	
 	# check vertical, y
 	for ($i=0; $i -lt 4; $i++) {
-		if ($i -ne $x -and $j -ne $y) {
-			#$board[$x][$i] = 'Q'
+		if ($board_copy[$x][$i] -eq 'Q') {
 			return $true
 		}
-		#return $board[$x][$i] -eq 'Q'
 	}
 	
 	# check diagonal
@@ -35,14 +40,16 @@ function CheckAttack ($x, $y) {
 				if (($i-$x) -eq 0) { break }
 				$m1 = ($j-$y)/($i-$x)
 				if ($m1 -eq 1 -or $m2 -eq -1) {
-					#$board[$i][$j] = 'Q'
-					return $true
+					if ($board_copy[$i][$j] -eq 'Q') {
+						return $true
+					}
 				}
 
 				$m2 = -($j-$y)/($i-$x)
 				if ($m1 -eq 1 -or $m2 -eq -1) {
-					#$board[$i][$j] = 'Q'
-					return $true
+					if ($board_copy[$x][$i] -eq 'Q') {
+						return $true
+					}
 				}
 
 			}
@@ -57,17 +64,21 @@ function TryQueen ($b, $i, $j) {
 	$board_copy = @() + $b
 	for ($x=$i; $x -lt 4; $x++) {
 		for ($y=$j; $y -lt 4; $y++) {
-			if ( CheckAttack $x $y -eq $false) {
+			if (-not (CheckAttack $board_copy $x $y)) {
 				#sleep .001
 				#'try queen'
-				#if ($x -ge 3) { TryQueen $board_copy ($x+1) $y }
-				#elseif ($y -lt 3) { TryQueen $board_copy 0 ($y+1) }
+				$board_copy[$x][$y] = 'Q'
+				if ($x -ge 3) { TryQueen $board_copy ($x+1) $y }
+				elseif ($y -lt 3) { TryQueen $board_copy 0 ($y+1) }
 
 				#"$x $y"
-				$board[$x][$y] = 'Q'
+
 			}
 		}
 	}
+
+	PrintBoard $board_copy
+	'--------'
 }
 
 
@@ -75,19 +86,12 @@ function TryQueen ($b, $i, $j) {
 
 
 
-function PrintBoard () {
-	for ($x=0; $x -lt 4; $x++) {
-		for ($y=0; $y -lt 4; $y++) {
-			[Console]::Write( $board[$x][$y] )
-		}
-		[Console]::Write( "`n" )
-	}
-}
+
 
 
 #CheckAttack 1 1
 TryQueen $board 0 0
-PrintBoard
+
 
 
 <#
